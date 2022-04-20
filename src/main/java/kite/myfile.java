8b7187d9862dc0b46433ee8b5b51e93b3cb6a939
sadanad <sada.sarokar@gmail.com>
2022-04-20 11:26:55 +0530

    my file added

---
+++ src/main/java/kite/myfile.java
@@ -0,0 +1,10 @@
+package kite;
+
+public class myfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/kite/state.java
+++ src/main/java/kite/state.java
@@ -1,5 +1,29 @@
 package kite;
 
+
+
 public class state {
+final  static public void main(String[] args) {
+		
+		main_method n = new main_method();
+		n.name1();
+		name2();
+		
+	}
+	
+	public void name1() {
+		
+		name2();
+		
+		
+		System.out.println("this is java");
+		
+	}
+
+	public static void name2() {
+		
+		System.out.println("this in pythan");
+		
+	}
 
 }
